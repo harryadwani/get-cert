92e55119fdbed2d83a88bf35d3ccbb5643689a85
harsh adwani <harryadwani9@gmail.com>
2023-01-22 16:34:34 +0530

    init

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,7 +1,7 @@
 module.exports = {
     apps: [{
-        name: "server",
-        script: "./server.js",
+        name: "main",
+        script: "./main.js",
         env: {
             NODE_ENV: "development"
         },
